8f8f88b07cffb697b6b60eabc7a98fe9b74e2560
unknown <genesis.arequipa1170@utc.edu.ec>
2026-06-04 18:44:12 -0500

    cambio 1

--- app/src/main/java/com/example/recuperaciongene/App.java
+++ app/src/main/java/com/example/recuperaciongene/App.java
@@ -3,12 +3,28 @@
  */
 package com.example.recuperaciongene;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+public class App {
+    
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double nota1, nota2, nota3, promedio;
+
+        System.out.print("Ingrese la primera nota: ");
+        nota1 = sc.nextDouble();
+
+        System.out.print("Ingrese la segunda nota: ");
+        nota2 = sc.nextDouble();
+
+        System.out.print("Ingrese la tercera nota: ");
+        nota3 = sc.nextDouble();
+
+        promedio = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("El promedio es: " + promedio);
+
+       
     }
 }
